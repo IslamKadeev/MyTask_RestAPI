8323cd793620c8e66e8fd7b77fd84883ebcaa850
IslamKadeev <kadeevkgeu@gmail.com>
2023-07-19 22:41:01 +0300

    Added db migrations via alembic

--- models/task.py
+++ models/task.py
@@ -2,11 +2,20 @@ from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, Fore
 
 metadata = MetaData()
 
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("position", String, nullable=False)
+)
+
 tasks = Table(
     "tasks",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("description", String, nullable=False),
-    Column("duedate", TIMESTAMP, nullable=True)
+    Column("duedate", TIMESTAMP, nullable=True),
+    Column("assignee_id", Integer, ForeignKey("users.id"))
 )
